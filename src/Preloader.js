3c11717b76ea2b18975b8057d2cb928e8032173c
Federico Haustein <fedehaust@gmail.com>
2016-10-17 17:43:30 -0300

    Add files via upload

---
+++ src/Preloader.js
@@ -0,0 +1,31 @@
+Ball.Preloader = function(game) {};
+Ball.Preloader.prototype = {
+	preload: function() {
+        this.levelText = this.game.add.text((Ball._WIDTH-65)*0.5, (Ball._HEIGHT)*0.5, "Cargando", { font: "16px Arial", fill: "#e4beef" });
+		//this.preloadBg = this.add.sprite((Ball._WIDTH-297)*0.5, (Ball._HEIGHT-145)*0.5, 'preloaderBg');
+		//this.preloadBar = this.add.sprite((Ball._WIDTH-158)*0.5, (Ball._HEIGHT-50)*0.5, 'preloaderBar');
+		//this.load.setPreloadSprite(this.preloadBar);
+
+		this.load.image('ball', 'img/ball.png');
+		this.load.image('hole', 'img/hole.png');
+		this.load.image('element-w', 'img/element-w.png');
+		this.load.image('element-h', 'img/element-h.png');
+        this.load.image('element-c', 'img/element-c.png');
+		this.load.image('panel', 'img/panel.png');
+		this.load.image('title', 'img/title.png');
+		this.load.image('button-pause', 'img/button-pause.png');
+        this.load.image('button-next', 'img/button-next.png');
+		this.load.image('screen-bg', 'img/screen-bg.png');
+		this.load.image('screen-mainmenu', 'img/screen-mainmenu.png');
+		this.load.image('border-horizontal', 'img/border-horizontal.png');
+		this.load.image('border-vertical', 'img/border-vertical.png');
+
+		this.load.spritesheet('button-audio', 'img/button-audio.png', 35, 35);
+		this.load.spritesheet('button-start', 'img/button-start.png', 146, 51);
+
+		this.load.audio('audio-bounce', ['audio/bounce.ogg', 'audio/bounce.mp3', 'audio/bounce.m4a']);
+	},
+	create: function() {
+		this.game.state.start('MainMenu');
+	}
+};
